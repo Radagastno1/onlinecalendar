abb09c957fc611d044293bc12fa1147c1d929feb
Elina Kerola <elinak90@icloud.com>
2023-06-12 12:16:57 +0200

    #added prev month and next month

---
+++ public/calendar.js
@@ -0,0 +1,10 @@
+// var monthYearElement = document.querySelector('[data-cy="month-year"]');
+
+// var currentDate = new Date();
+
+// function changeMonth(offset) {
+//     currentDate.setMonth(currentDate.getMonth() + offset);
+// }
+
+// var month = currentDate.toLocaleString('sv-SE', { month: 'long'});
+// var year = currentDate.getFullYear();
