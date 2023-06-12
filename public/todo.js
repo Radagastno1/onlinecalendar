7e1aeb230be227b081946fe31502e5bbc1b938f9
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-06-12 11:45:59 +0200

    added functions for showing list of todos, saving todo and generate date. also a function for showing list of todos when clicking on button

--- public/todo.js
+++ public/todo.js
@@ -1,3 +1,6 @@
+let todoList = [
+];
+
 export function showAddTodoForm() {
   const addTodoButton = document.querySelector('#add-todo-btn');
   const addTodoForm = document.querySelector('#add-todo-form');
@@ -5,4 +8,72 @@ export function showAddTodoForm() {
   addTodoButton.addEventListener('click', () => {
     addTodoForm.classList.toggle('todo-reveal-form');
   });
+}
+
+export function showListOfTodos() {
+  const showTodosButton = document.querySelector('#show-todos-btn');
+  const todosUl = document.querySelector('#todo-list');
+
+  showTodosButton.addEventListener('click', () => {
+    todosUl.classList.toggle('todo-reveal-list');
+  });
+}
+
+export function addEventListeners() {
+  const saveButton = document.querySelector('#save-todo-button');
+  saveButton.addEventListener('click', saveTodo);
+}
+
+export function renderTodoList() {
+  const todoUl = document.querySelector('#todo-list');
+
+  for (const todo of todoList) {
+    const li = document.createElement('li');
+    li.textContent = todo.title;
+    todoUl.appendChild(li);
+  }
+  if(todoList.length == 0)
+  {
+    const li = document.createElement('li');
+    li.textContent = "Det finns ingen todo än.."
+    todoUl.appendChild(li);
+  }
+}
+
+function saveTodo() {
+  const todoTitle = document.querySelector('#todo-title-input').value;
+  const todoDate = document.querySelector('#todo-date-input').value;
+
+  const todo = {
+    title: todoTitle,
+    date: todoDate
+  };
+
+  todoList.push(todo);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+function getTodaysDate() {
+  var today = new Date();
+  today = generateDateFormat(today);
+  return today;
+}
+
+function generateDateFormat(date) {
+
+  var dd = date.getDate();
+
+  var mm = date.getMonth() + 1;
+
+  var yyyy = date.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  formatedDate = yyyy + '-' + mm + '-' + dd;
+  return formatedDate;
 }
