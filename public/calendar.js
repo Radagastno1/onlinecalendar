059c52eb88e81d7eb9c64857ea5ef3bf6b116d94
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-06-16 17:16:56 +0200

    commiting in  main

--- public/calendar.js
+++ public/calendar.js
@@ -5,6 +5,9 @@ const state = {
 }
 
 function initCalendar() {
+
+  updateCalendarCells();
+
   var prevMonthButton = document.querySelector("[data-cy='prev-month']");
   var nextMonthButton = document.querySelector("[data-cy='next-month']");
 
@@ -16,31 +19,17 @@ function initCalendar() {
     changeMonth(1);
   });
 
-  updateCalendarCells();
-
   addCalendarCellListeners();
 }
-// bytte till denna så man kan klicka på cellen för att se todosen igen
-function addCalendarCellListeners() {
-    const calendarCells = document.querySelectorAll("[data-cy='calendar-cell']");
-    calendarCells.forEach(calendarCell => {
-      calendarCell.addEventListener('click', event => {
-        const day = calendarCell.querySelector("[data-cy='calendar-cell-date']").textContent;
-        const filteredTodos = getTodosForDay(state.year, state.month, parseInt(day));
-        renderFilteredTodoList(filteredTodos);
-      });
-    });
-  }
-  
 
-// function addCalendarCellListeners() {
-//   const calendarCells = document.querySelectorAll("[data-cy='calendar-cell']");
-//   calendarCells.forEach(calendarCell => {
-//     calendarCell.addEventListener('click', event => {
-//       filterTodoByCalendarCell(event);
-//     });
-//   });
-// }
+function addCalendarCellListeners() {
+  const calendarCells = document.querySelectorAll("[data-cy='calendar-cell']");
+  calendarCells.forEach(calendarCell => {
+    calendarCell.addEventListener('click', event => {
+      filterTodoByCalendarCell(event);
+    });
+  });
+}
 
 function changeMonth(change) {
   const { month, year } = state;
@@ -86,8 +75,59 @@ function getMonthNumber(month) {
   return months.indexOf(month.toLowerCase()) + 1;
 }
 
+// function updateCalendarCells() {
+//   const { month, year } = state;
+
+//   const currentDate = new Date();
+//   const currentDay = currentDate.getDate();
+
+//   const firstDayOfMonth = new Date(year, month, 1);
+//   const startingDay = (firstDayOfMonth.getDay() + 6) % 7;
+
+//   const daysInMonth = new Date(year, month + 1, 0).getDate();
+//   let dayCounter = 1;
+
+//   const calendarBody = document.getElementById('calendar-body');
+//   const rows = calendarBody.getElementsByTagName('tr');
+
+//   for (let i = 0; i < rows.length; i++) {
+//     const cells = rows[i].getElementsByTagName('td');
+
+//     for (let j = 0; j < cells.length; j++) {
+//       const cell = cells[j];
+
+//       if (i === 0 && j < startingDay) {
+//         cell.textContent = '';
+//       } else if (dayCounter > daysInMonth) {
+//         cell.textContent = '';
+//       } else {
+//         const dateElement = document.createElement('span');
+//         dateElement.textContent = dayCounter;
+//         dateElement.setAttribute('data-cy', 'calendar-cell-date');
+
+//         // Ta bort den befintliga textnoden om den finns
+//         while (cell.firstChild) {
+//           cell.firstChild.remove();
+//         }
+
+//         cell.appendChild(dateElement);
+
+//         if (
+//           dayCounter === currentDay &&
+//           month === currentDate.getMonth() &&
+//           year === currentDate.getFullYear()
+//         ) {
+//           cell.classList.add('current-day');
+//         }
+
+//         dayCounter++;
+//       }
+//     }
+//   }
+// }
 
 function updateCalendarCells() {
+
   var calendarBody = document.getElementById('calendar-body');
   calendarBody.innerHTML = '';
 
@@ -102,15 +142,28 @@ function updateCalendarCells() {
   var daysInMonth = new Date(year, month + 1, 0).getDate();
   var dayCounter = 1;
 
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+
+  var daysOfWeek = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
+
+  for (var j = 0; j < 7; j++) {
+    var th = document.createElement('th');
+    th.textContent = daysOfWeek[j];
+    tr.appendChild(th);
+  }
+
+  thead.appendChild(tr);
+  table.appendChild(thead);
+  calendarBody.appendChild(table);
+
   for (var i = 0; i < 6; i++) {
     var row = document.createElement('tr');
 
     for (var j = 0; j < 7; j++) {
       var cell = document.createElement('td');
       cell.setAttribute('data-cy', 'calendar-cell');
-
-      // Hämta antalet todos för dagen
-      var todosForDay = getTodosForDay(year, month, dayCounter);
 
       if (i === 0 && j < startingDay) {
         cell.textContent = '';
@@ -122,13 +175,6 @@ function updateCalendarCells() {
         dateElement.setAttribute('data-cy', 'calendar-cell-date');
         cell.appendChild(dateElement);
 
-        // Lägg till siffran för antalet todos i kalendercellen
-        if (todosForDay.length > 0) {
-          var todosCountElement = document.createElement('span');
-          todosCountElement.textContent = todosForDay.length;
-          todosCountElement.setAttribute('data-cy', 'calendar-cell-todos');
-          cell.appendChild(todosCountElement);
-        }
         if (dayCounter === currentDay && month === currentDate.getMonth() && year == currentDate.getFullYear()) {
           cell.classList.add('current-day');
         }
@@ -139,29 +185,9 @@ function updateCalendarCells() {
       row.appendChild(cell);
     }
 
-    calendarBody.appendChild(row);
-
+    table.appendChild(row);
   }
 }
-
-function getTodosForDay(year, month, day) {
-  var todosForDay = [];
-
-  for (const todo of todoList) {
-    var todoDate = new Date(todo.date);
-    if (
-      todoDate.getFullYear() === year &&
-      todoDate.getMonth() === month &&
-      todoDate.getDate() === day
-    ) {
-      todosForDay.push(todo);
-    }
-  }
-
-  return todosForDay;
-}
-
-
 
 
 
