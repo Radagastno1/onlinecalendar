cbf0e486ca2aef76e5a35f759ddefe682f19a774
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-06-22 11:39:17 +0200

    pushing finished main

--- public/main.js
+++ public/main.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
   todoList = getDataFromLS();
-  //anropar funktioner från calendar.js
+
   initCalendar();
 
-  //anropar funktioner från todo
   initTodos();
 
-  //anropar funktioner från today
   initToday();
 }
